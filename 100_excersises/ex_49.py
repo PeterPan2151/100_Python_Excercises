b12c1ddc2835b2c7f676d2b9c469b451890752d8
Salvador Montiel <126306642+PeterPan2151@users.noreply.github.com>
2025-08-21 19:28:55 -0600

    Add files via upload

---
+++ 100_excersises/ex_49.py
@@ -0,0 +1,21 @@
+# Define a class named Shape and its subclass Square. The Square class has an init function which takes a length as argument. 
+# Both classes have a area function which can print the area of the shape where Shape's area is 0 by default.
+class Shape:
+    def __init__(self):
+        pass
+
+    def compute_area(self):
+        return 0
+
+class Square(Shape):
+    def __init__(self, length=0):
+        Shape.__init__(self)
+        self.length = length
+
+    def compute_area(self):
+        return self.length * self.length
+    
+Asqr = Square(5)
+print(Asqr.compute_area())      
+
+print(Square().compute_area()) 
