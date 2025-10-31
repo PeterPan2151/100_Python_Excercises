e9ea5d1cd2bdac2228ab7934e6cf8fd1975a9611
Salvador Montiel <126306642+PeterPan2151@users.noreply.github.com>
2025-10-30 23:42:17 -0600

    Add files via upload

---
+++ 100_excersises/ex_60.py
@@ -0,0 +1,16 @@
+# Write a program to compute:
+# f(n)=f(n-1)+100 when n>0
+# and f(0)=0
+# with a given n input by console (n>0).
+# Example: If the following n is given as input to the program:
+# 5
+# Then, the output of the program should be:
+# 500
+
+def compute(n):
+    if n == 0:
+        return 0
+    else:
+        return compute(n-1) + 100
+    
+print(compute(5))
